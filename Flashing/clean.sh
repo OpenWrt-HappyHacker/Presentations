4e8885f283c976d39e5df30901c3f788856ecf51
Count Crapula <crapula@alligatorcon.pl>
2017-03-12 23:03:24 +0100

    Video instructions on how to flash firmware images to the ZSUN

---
+++ Flashing/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+killall  php-cgi
+killall  smbd   
+killall  nmbd
+killall udhcpd
+killall webs  
+killall clearlog
+killall dnsserver
+killall lighttpd 
+killall boa     
+killall tfcheck
+killall wifi_daemon 
+killall upsamb
+killall daemon
